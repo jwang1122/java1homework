5aeb3e3d553b714ff254c7a35fae823aa6f7437e
wangqianjiang <wangqianjiang@192.168.1.12>
2020-07-20 09:51:20 -0500

    turn in kaiser's homework

---
+++ kaiser/src/com/huaxia/kaiser/Math12023.java
@@ -0,0 +1,12 @@
+package com.huaxia.kaiser;
+
+public class Math12023 {
+
+	public static void main(String[] args) {
+		System.out.println("01: 2020-20="+(2020-20));
+		System.out.println("02: 494+118="+(494+118));
+
+
+	}
+
+}
