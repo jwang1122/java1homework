897b93f61575b8982de85838809bbdbfe8b0628d
jacob <jacob@10.0.0.30>
2020-09-08 19:11:43 -0500

    Math Homework

--- jacob/src/com/huaxia/jacob/Math12023.java
+++ jacob/src/com/huaxia/jacob/Math12023.java
@@ -4,6 +4,9 @@ public class Math12023 {
 
 	public static void main(String[] args) {
 		System.out.printf("01. %d - %d = %d\n", 2020, 20, (2020-20));
+		System.out.printf("02. %d + %d = %d\n", 494, 188, (494-188));
+		System.out.println("I don't know how to do it.");
+
 	}
 
 }
