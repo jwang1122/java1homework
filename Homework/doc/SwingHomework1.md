2c7f2ea9f512f236b43e9e37fb24e43dc02af9ed
John Wang <wangqianjiang@Johns-MacBook-Pro.local>
2020-09-22 15:59:20 -0500

    add  Swing Homework-1

---
+++ Homework/doc/SwingHomework1.md
@@ -0,0 +1,7 @@
+# Java Swing Homework-1
+
+* Write a simple math window application
+* build jar in desktop
+* the application should look like
+
+![Smple Math](SimpleMath.png)
--- John/src/com/huaxia/john/Math12023.java
+++ John/src/com/huaxia/john/Math12023.java
@@ -5,7 +5,14 @@ public class Math12023 {
 	public static void main(String[] args) {
 		System.out.println("01: 2020-20="+(2020-20));
 		System.out.println("02: 494+118="+(494+118));
-
+		int a = 2020;
+		int b = 20;
+		int c = a - b;
+		System.out.printf("01. %d - %d = %d\n", a, b, c);
+		a = 494;
+		b = 118;
+		c = a + b;
+		System.out.printf("02. %d + %d = %d\n", a, b, c);
 	}
 
 }
